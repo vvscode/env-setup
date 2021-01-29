e91db96eeaf135ff358ba7eb2fdd4dc2be4e693f
Vasil Vanchuk <v.vanchuk@tut.by>
2021-01-29 20:13:16 +0200

    Setup eslint-jest plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
   ],
+  plugins: ['jest'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
